1964416d9fea9b7c961afb32e33ee61704e901d0
gustavogss <gustavogss.dev@gmail.com>
2023-03-09 01:02:12 -0300

    Configurando a OpenAI - Backend

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -1,17 +1,24 @@
 const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+module.exports = class openai{
+    static configuration(){
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+          });
+        return new OpenAIApi(configuration);
+    }
 
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
-  temperature: 0.9,
-  max_tokens: 150,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0.6,
-  stop: [" Human:", " AI:"],
-});
+    static textCompletion({prompt}){
+        return {                        
+                model: "text-davinci-003", //nome_do_algoritmo
+                prompt: `${prompt}`,
+                temperature: 0.9,
+                max_tokens: 150,
+                top_p: 1,
+                frequency_penalty: 0.5,
+                presence_penalty: 0.6,
+                stop: [" Human:", " AI:"],            
+        }
+    }
+}
