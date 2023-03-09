a6f73c7131b9a03414ff44905c2023a07a37b11a
gustavogss <gustavogss.dev@gmail.com>
2023-03-09 01:39:22 -0300

    Integrando a OpenAI a aplicação

--- server/src/controller/prompt-controller.js
+++ server/src/controller/prompt-controller.js
@@ -1,5 +1,24 @@
+const openai = require('../config/openai');
+const InputPrompt = require('../model/input-model');
+
 module.exports = {
-    async sendText(request, response) {
-        return response.status(200).json({message:"Resposta enviada"});
+    async sendText(req, res) {
+        const openaiAPI = openai.configuration();
+        const inputModel = new InputPrompt(req.body);
+        try {
+            const result = await openaiAPI.createCompletion(
+                openai.textCompletion(inputModel)
+            );
+            return res.status(200).json({
+                sucess: true,
+                data: result.data.choices[0].text
+            });
+        } catch (error) {
+            return res.status(400).json({
+                sucess: false,
+                error: error.result ? error.result.data : "Error Server"
+            });
+        }
+        
 }
 };
